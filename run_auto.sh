da82ec05a2826e6b5f63be43ee73141764572d98
jsshin <musimain@naver.com>
2025-09-08 14:16:17 +0900

    최종 update

--- run_auto.sh
+++ run_auto.sh
@@ -2,7 +2,7 @@
 cd /Volumes/SSD/RESTAPI
 source /Volumes/SSD/RESTAPI/venv311/bin/activate
 pkill -f "/Volumes/SSD/RESTAPI/venv311/bin/python3 /Volumes/SSD/RESTAPI/trading_system/main.py"
-cd /Volumes/SSD/RESTAPI/trading_system
-/Volumes/SSD/RESTAPI/venv311/bin/python3 /Volumes/SSD/RESTAPI/trading_system/main.py
 cd /Volumes/SSD/RESTAPI/trading_system/monitoring
-/Volumes/SSD/RESTAPI/venv311/bin/python3 /Volumes/SSD/RESTAPI/trading_system/monitoring/monitoring_server.py
+/Volumes/SSD/RESTAPI/venv311/bin/python3 /Volumes/SSD/RESTAPI/trading_system/monitoring/monitoring_server.py &
+cd /Volumes/SSD/RESTAPI/trading_system
+/Volumes/SSD/RESTAPI/venv311/bin/python3 /Volumes/SSD/RESTAPI/trading_system/main.py  
